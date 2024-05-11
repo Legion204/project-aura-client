b8c61f0f26a1e11c0696de54803ec82ccc4645b0
Legion204 <ri46801@gmail.com>
2024-05-11 06:51:00 +0600

    added food card and featured foods section in home page

---
+++ src/Components/FoodCard.jsx
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom";
+import { TbPackages } from "react-icons/tb";
+import { FaLocationDot } from "react-icons/fa6";
+import { MdNoteAlt } from "react-icons/md";
+import PropTypes from 'prop-types';
+
+const FoodCard = ({ featuredFood }) => {
+    const { foodName, imageUrl, pickupLocation, quantity, exp_date, donatorName, donatorImg, additional_notes } = featuredFood;
+    return (
+        <div className="justify-self-center">
+            <div className="card card-compact w-96 lg:w-[450px] bg-base-100 shadow-xl font-Source">
+                <figure><img src={imageUrl} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title text-3xl">{foodName}</h2>
+                    <div className="flex justify-between items-center">
+                        <h1 className="text-2xl text-third">Donated by:</h1>
+                        <div className="text-center">
+                            <div className="avatar">
+                                <div className="w-14 rounded-xl">
+                                    <img src={donatorImg} />
+                                </div>
+                            </div>
+                            <h1 className="text-xl">{donatorName}</h1>
+                        </div>
+                    </div>
+                    <div className=" font-medium">
+                        <p className="flex items-center text-lg text-gray-500"><span className="text-third text-lg mr-2"><FaLocationDot /></span>Pickup Location: {pickupLocation}</p>
+                        <p className="text-lg text-gray-500 flex items-center"><span className="mr-2 text-third"><TbPackages /></span>Quantity: {quantity}</p>
+                        <p className="text-lg text-gray-500 lg:col-span-2 flex  items-center"><span className="mr-2 text-third"><MdNoteAlt /></span>Additional Notes: {additional_notes}</p>
+                    </div>
+                    <div className="card-actions items-center">
+                        <Link><button className="btn bg-third text-white">View details</button></Link>
+                        <p className="flex flex-col text-start text-xl font-medium pl-24 2xl:pl-44">Exp Date <span className="text-third">{exp_date}</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+FoodCard.propTypes={
+    featuredFood:PropTypes.object
+}
+
+export default FoodCard;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,43 @@
+import { useEffect, useState } from "react";
 import Banner from "../Components/Banner";
+import useAxiosSecure from "../Hooks/useAxiosSecure";
+import FoodCard from "../Components/FoodCard";
 
 
 
 const Home = () => {
+
+    const axiosSecure = useAxiosSecure();
+    const [featuredFoods, setFeaturedFoods] = useState([]);
+
+    useEffect(() => {
+        axiosSecure.get('/featured_foods')
+            .then(data => {
+                setFeaturedFoods(data.data);
+                console.log(data.data);
+            })
+    }, [axiosSecure]);
+
+
     return (
         <div>
             {/* Banner section */}
             <Banner></Banner>
+            {/* Featured Foods section */}
+            <div className="grid">
+                <div className=" w-2/4 ml-6 my-10 space-y-3 font-Poetsen justify-self-center text-center" >
+                    <h1 className="font-extrabold text-5xl">Featured Foods</h1>
+                    <p className="">Welcome to our Featured Foods section, where the essence of community shines through every meal. Here, you all discover a delightful assortment of dishes generously donated by our community members.</p>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-10">
+                    {
+                        featuredFoods.slice(0, 6).map(featuredFood => <FoodCard
+                            key={featuredFood._id}
+                            featuredFood={featuredFood}
+                        ></FoodCard>)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
