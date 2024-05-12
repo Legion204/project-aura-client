295c53f96d8669d787a20edff7772fb7978a9c03
Legion204 <ri46801@gmail.com>
2024-05-12 09:07:26 +0600

    fixed search funtion and added ascending sort

--- src/Pages/AvailableFoodsPage.jsx
+++ src/Pages/AvailableFoodsPage.jsx
@@ -15,8 +15,8 @@ const AvailableFoodsPage = () => {
             })
     }, [axiosSecure]);
 
-
-    const handelSort = () => {
+    // sort by exp date descending order
+    const handelSortDisc = () => {
         const sortedDateFood = [...availableFoods].sort((x, y) => {
             x = new Date(x.exp_date),
                 y = new Date(y.exp_date);
@@ -25,12 +25,17 @@ const AvailableFoodsPage = () => {
         });
         setAvailableFoods(sortedDateFood);
     };
+    
+    // sort by exp date ascending order
 
-    const handelSearch = () => {
-        const filteredFood = [...availableFoods].filter((availableFood) =>
-            availableFood?.foodName.trim().toLowerCase().includes(foodName.toLowerCase())
-        );
-        setAvailableFoods(filteredFood);
+    const handelSortAsc = () => {
+        const sortedDateFood = [...availableFoods].sort((x, y) => {
+            x = new Date(x.exp_date),
+                y = new Date(y.exp_date);
+
+            return x - y;
+        });
+        setAvailableFoods(sortedDateFood);
     };
 
     return (
@@ -39,26 +44,24 @@ const AvailableFoodsPage = () => {
                 <h1 className="font-extrabold text-5xl">Available Foods</h1>
                 <p className="">Welcome to our Available Foods page, where the essence of community shines through every meal. Here, you all discover a delightful assortment of dishes generously donated by our community members.</p>
             </div>
-            <div className="join justify-self-center lg:justify-self-end lg:mr-10">
-                <div>
-                    <div>
-                        <input className="input input-bordered join-item" placeholder="Search" type="text" name="search" onChange={(e) => setFoodName(e.target.value)} />
-                    </div>
-                </div>
-                <details className="dropdown ">
-                    <summary className=" btn bg-third text-white rounded-none">Sort by</summary>
+            <div className=" flex justify-center lg:justify-end gap-5 lg:mr-10">
+                <label className="input input-bordered flex items-center gap-2">
+                    <input type="text" className="grow" placeholder="Search" onChange={(e) => { setFoodName(e.target.value) }} />
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+                </label>
+                <details className="dropdown dropdown-bottom dropdown-end ">
+                    <summary className=" btn bg-third text-white">Sort by Expire Date</summary>
                     <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-                        <li><a onClick={handelSort}>Expire Date</a></li>
-                        <li><a>Item 2</a></li>
+                        <li><a onClick={handelSortAsc}>Ascending order</a></li>
+                        <li><a onClick={handelSortDisc}> Descending order</a></li>
                     </ul>
                 </details>
-                <div className="indicator">
-                    <button onClick={handelSearch} className="btn bg-third text-white join-item">Search</button>
-                </div>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-10 mt-12">
                 {
-                    availableFoods.map(availableFood => <FoodCard
+                    availableFoods.filter((availableFood) =>
+                        availableFood?.foodName.trim().toLowerCase().includes(foodName.toLowerCase())
+                    ).map(availableFood => <FoodCard
                         key={availableFood._id}
                         featuredFood={availableFood}
                     ></FoodCard>)
